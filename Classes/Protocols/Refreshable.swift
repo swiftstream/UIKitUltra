561ae57fb25ce67f05b723a8ff38ff363367a71f
Mikhail Isaev <isaev.mihael@gmail.com>
2026-06-20 03:52:57 +0400

    🛠 Clarify refreshable state binding holds

--- Classes/Protocols/Refreshable.swift
+++ Classes/Protocols/Refreshable.swift
@@ -9,55 +9,55 @@ extension Refreshable {
     @discardableResult
     public func react<A>(to a: State<A>) -> Self {
         a.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         return self
     }
     
     @discardableResult
     public func react<A, B>(to a: State<A>, _ b: State<B>) -> Self {
         a.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         b.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         return self
     }
     
     @discardableResult
     public func react<A, B, C>(to a: State<A>, _ b: State<B>, _ c: State<C>) -> Self {
         a.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         b.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         c.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         return self
     }
     
     @discardableResult
     public func react<A, B, C, D>(to a: State<A>, _ b: State<B>, _ c: State<C>, _ d: State<D>) -> Self {
         a.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         b.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         c.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         d.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         return self
     }
     
     @discardableResult
     public func react<A, B, C, D, E>(to a: State<A>, _ b: State<B>, _ c: State<C>, _ d: State<D>, _ e: State<E>) -> Self {
         a.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         b.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         c.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         d.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         e.listen { [weak self] _, _ in self?.refresh() }
-            .holdIfOwned(by: self)
+            .holdInStateBindingOwnerIfAvailable(self)
         return self
     }
 }
